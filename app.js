8acb48a79d3e4b675ab8bb1f577f92eb0a1e2c23
Daniel <danielhjermitslev@protonmail.com>
2020-11-13 09:07:52 +0100

    add for loop

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var observeMe = document.getElementsByClassName("observeMe");
 
 var observer = new IntersectionObserver(function (entries) {
-    if (entries[0].intersectionRatio == 0) return;
+    if (entries[0].intersectionRatio == 0) return; // Vi stopper hermed vores intersectionObserver fra at køre når vi loader siden
 
     entries[0].target.style.backgroundColor = 'teal';
 }, {
     threshold: 1 // Bestemmer hvor meget elementet skal være inde i viewporten før at trigger
 })
+
+for (let i = 0; i < observeMe.length; i = i + 2) {
+    observer.observe(observeMe[i]);
+}
